b5b183f1d354defc08ca1604807ed68ebafb9bfa
awaller0 <amwaller04@gmail.com>
2023-11-13 16:53:18 -0800

    completed displayAllWords function

--- scrabbleHelper.cpp
+++ scrabbleHelper.cpp
@@ -1,23 +1,9 @@
 #include "scrabbleHelper.h"
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <fstream>
-
-using namespace std;
-
 
 int main()
 {
     ScrabbleHelper helper;
-    helper.displayWordsWithPrefix("help");
-    helper.displayWordsWithSuffix("ing");
-
-
-
-
+    helper.readLettersFromUser();
+    helper.displayPossibleWords("","");
     return 0;
 }
-
-
--- scrabbleHelper.h
+++ scrabbleHelper.h
@@ -4,13 +4,14 @@
 #include <sstream>
 #include <cctype>
 #include <fstream>
+#include <algorithm>
 using namespace std;
 #ifndef scrabbleHelper_H
 #define scrabbleHelper_H
 
 // TODO: check if the user's input is valid through checking if the letters are in the alphabet as well as if the letters are in the right format
 // TODO: add function to search if a word contains certain letters in the order typed by the user
-
+// TODO: add function that takes 3 arguements, the user's letters, the prefix, and the suffix and returns all the words that contain the prefix and suffix with the user's letters
 class ScrabbleHelper
 {
 private:
@@ -125,8 +126,8 @@ public:
 
     void displayWordsWithSuffix(const string &suffix)
     {
-        string reversedSuffix = string(suffix.rbegin(), suffix.rend());          // Reverse the suffix EX: user: ing, reversedSuffix://
-        vector<string> words = prefixSearchInternal(reversedSuffix, reversedValidWords); // gni, since the words are stored in reverse order//
+        string reversedSuffix = string(suffix.rbegin(), suffix.rend());                  // Reverse the suffix EX: user: ing, reversedSuffix://
+        vector<string> words = prefixSearchInternal(reversedSuffix, reversedValidWords); // gni, since the words are stored in reverse order //
         if (words.empty())
         {
             cout << "No words with that suffix" << endl;
@@ -136,9 +137,100 @@ public:
         cout << "Words found with that suffix" << endl;
         for (const auto &word : words)
         {
-            string reversdWord = string(word.rbegin(), word.rend()); // reversing the words back to normal
-            cout << reversdWord << endl;
+            string reversedWord = string(word.rbegin(), word.rend()); // reversing the words back to normal
+            cout << reversedWord << endl;                             // printing the words
         }
+    }
+
+    void displayPossibleWords(const string &prefix, const string &suffix)
+    {
+        // Step 1: Find all words with the given prefix.
+        vector<string> prefixWords = prefixSearchInternal(prefix, validWords);
+
+        // Step 2: Find all words with the given suffix, using reversed order search.
+        string reversedSuffix = string(suffix.rbegin(), suffix.rend());
+        vector<string> suffixWordsTemp = prefixSearchInternal(reversedSuffix, reversedValidWords);
+        vector<string> suffixWords; // This will store the suffix words in the correct order.
+
+        for (auto &word : suffixWordsTemp)
+        {
+            suffixWords.push_back(string(word.rbegin(), word.rend()));
+        }
+        // Step 3: Sort the two vectors.
+        sort(prefixWords.begin(), prefixWords.end());
+        sort(suffixWords.begin(), suffixWords.end());
+
+        // Step 4: Intersect the two vectors.
+        vector<string> commonWords;
+
+
+        set_intersection(prefixWords.begin(), prefixWords.end(),
+                         suffixWords.begin(), suffixWords.end(),
+                         back_inserter(commonWords));
+
+
+
+        vector<int> userInputFreq = countUserLetters();
+        vector<string> possibleWords;
+
+        for (const string &word : commonWords)
+        { // Assume validWords is your list of valid Scrabble words
+            vector<int> wordFreq = countWordLetters(word);
+
+            if (isPossibleWord(userInputFreq, wordFreq))
+            {
+                possibleWords.push_back(word);
+            }
+        }
+        sort(possibleWords.begin(), possibleWords.end());
+        // Display the possible words
+        for (const string &word : possibleWords)
+        {
+            cout << word << endl;
+        }
+    }
+
+    bool isPossibleWord(const vector<int> &userInputFreq, const vector<int> &wordFreq)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (userInputFreq[i] < wordFreq[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    vector<int> countUserLetters()
+    {
+        vector<int> letterCount(26, 0); // Initialize array with 26 zeros
+
+        for (const string &letter : userLetters)
+        {
+            if (!letter.empty() && isalpha(letter[0]))
+            {                                // Check if the string is not empty and the first character is a letter
+                char c = tolower(letter[0]); // Convert the first character to lowercase
+                letterCount[c - 'a']++;      // Increment the count for this letter
+            }
+        }
+
+        return letterCount;
+    }
+    vector<int> countWordLetters(const string &word)
+    {
+        vector<int> letterCount(26, 0); // Initialize array with 26 zeros
+
+        for (char c : word)
+        {
+            if (isalpha(c))
+            {
+                c = tolower(c);         // Convert to lowercase
+                letterCount[c - 'a']++; // Increment the count
+            }
+        }
+
+        return letterCount;
     }
 };
 #endif
